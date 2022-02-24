22cda5f9ba5c062bd5566c1fa7cb71f0cf20e978
김영환 <gim-yeonghwan@gim-yeonghwan-ui-M1-Pro-MacBookPro.local>
2022-02-24 10:25:43 +0900

    eslint 라이브러리 추가 및 규칙 설정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from "@angular/forms"; // ngModule 은 이 패키지가 제공합니다.
+import { FormsModule } from '@angular/forms'; // ngModule 은 이 패키지가 제공합니다.
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, HeroesComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
